26287b166dd5f63eafe4ffed58bff6c3ca8bedf2
luc <luc@forgotten.name>
2023-12-30 17:29:43 -0500

    Added openai api call

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use openai_api_rs::v1::api::Client;
+use openai_api_rs::v1::chat_completion::{self, ChatCompletionRequest};
+use openai_api_rs::v1::common::GPT4;
+use std::env;
 use uuid::Uuid;
 
 #[derive(Debug)]
@@ -23,6 +27,31 @@ struct Node {
     label: String,
 }
 
+fn get_openai_response(prompt: String) -> Result<String, Box<dyn std::error::Error>> {
+    let client = Client::new(env::var("OPENAI_API_KEY")?);
+
+    let req = ChatCompletionRequest::new(
+        GPT4.to_string(),
+        vec![chat_completion::ChatCompletionMessage {
+            role: chat_completion::MessageRole::user,
+            content: prompt,
+            name: None,
+            function_call: None,
+        }],
+    );
+
+    let result = client.chat_completion(req)?;
+
+    // Handling the Option<String> with ok_or
+    result.choices.get(0)
+        .and_then(|choice| choice.message.content.clone())
+        .ok_or_else(|| "No response from OpenAI".into()) // Converting to Result
+}
+
 fn main() {
     println!("Hello, world!");
+    get_openai_response(String::from("What is an ontology?"))
+        .map(|response| println!("{}", response))
+        .map_err(|_| println!("Error with OpenAI API"))
+        .unwrap_or(());
 }
