3c3a4d8e88e1078a5e77f6bf52b9dd4321d1347f
luc <luc@forgotten.name>
2023-12-30 16:53:09 -0500

    Added Citation, Hyperedge, and Node structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use uuid::Uuid;
+
+#[derive(Debug)]
+struct Citation {
+    id: Uuid,
+    title: String,
+    label: String,
+    text: String,
+}
+
+#[derive(Debug)]
+struct Hyperedge {
+    id: Uuid,
+    label: String,
+    source: Vec<Node>,
+    target: Vec<Node>,
+    citations: Vec<Citation>,
+}
+
+#[derive(Debug)]
+struct Node {
+    id: Uuid,
+    label: String,
+}
+
 fn main() {
     println!("Hello, world!");
 }
